feb5395dd0446ee71a3ac5badc15149beeab751b
Andrew Cholakian <andrew@andrewvc.com>
2011-05-22 22:46:05 -0700

    Update deps, fix -s option

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
 (defproject parbench "1.0.2"
   :description "Parallel HTTP Visualizer"
   :main parbench.core
-  :jvm-opts ["-server"]
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [aleph "0.1.5-SNAPSHOT"]
+                 [aleph "0.2.0-alpha1"]
                  [diamondap/clj-apache-https  "1.0"]
                  [org.clojars.automata/rosado.processing "1.1.0"]])
